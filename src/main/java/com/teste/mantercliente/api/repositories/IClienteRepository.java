6b3bef1347484f070df046409c7273ebf7a462c9
VitorAlfeu <vitorelioterio@hotmail.com>
2023-09-26 23:49:00 -0300

    Adicionando uma query Nativa e Javadoc
    - Foi adicionada uma Query Nativa para fim de exemplo ao buscar RG;
    - Foi adicionado Javadocs para a maior compreensão da funcionalidade de cada método;

--- src/main/java/com/teste/mantercliente/api/repositories/IClienteRepository.java
+++ src/main/java/com/teste/mantercliente/api/repositories/IClienteRepository.java
@@ -3,15 +3,38 @@ package com.teste.mantercliente.api.repositories;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.teste.mantercliente.api.entities.Cliente;
 
+/**
+ * Esta interface define os métodos de consulta para a entidade Cliente utilizando Spring Data JPA.
+ */
 public interface IClienteRepository extends JpaRepository<Cliente, Long> {
 
-	Optional<Cliente> findByNome(String nome);
+    /**
+     * Busca um cliente pelo nome.
+     *
+     * @param nome O nome do cliente a ser pesquisado.
+     * @return Um objeto Optional que pode conter o cliente encontrado, ou vazio se não encontrado.
+     */
+    Optional<Cliente> findByNome(String nome);
 
-	Optional<Cliente> findByRg(String rg);
-	
-	Optional<Cliente> findByNomeAndRg(String nome, String rg);
-	
+    /**
+     * Executa uma query nativa para buscar um cliente pelo RG.
+     *
+     * @param rg O RG do cliente a ser pesquisado.
+     * @return Um objeto Optional que pode conter o cliente encontrado, ou vazio se não encontrado.
+     */
+    @Query(value = "SELECT * FROM clientes WHERE rg = ?1%", nativeQuery = true)
+    Optional<Cliente> findByRg(String rg);
+
+    /**
+     * Busca um cliente pelo nome e RG.
+     *
+     * @param nome O nome do cliente a ser pesquisado.
+     * @param rg   O RG do cliente a ser pesquisado.
+     * @return Um objeto Optional que pode conter o cliente encontrado, ou vazio se não encontrado.
+     */
+    Optional<Cliente> findByNomeAndRg(String nome, String rg);
 }
