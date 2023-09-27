ddd4a1135ac836f99e249a679ede7408d4accf0f
VitorAlfeu <vitorelioterio@hotmail.com>
2023-09-26 23:39:06 -0300

    Adicionando Javadoc ao clienteRepositoryJDBC

--- src/main/java/com/teste/mantercliente/api/repositories/impl/ClienteRepositoryJDBC.java
+++ src/main/java/com/teste/mantercliente/api/repositories/impl/ClienteRepositoryJDBC.java
@@ -9,6 +9,10 @@ import org.springframework.stereotype.Repository;
 import com.teste.mantercliente.api.entities.Cliente;
 import com.teste.mantercliente.api.repositories.IClienteRepositoryJDBC;
 
+/**
+ * Implementação da interface IClienteRepositoryJDBC que fornece métodos para acessar dados de clientes
+ * usando o JDBC (Java Database Connectivity).
+ */
 @Repository
 public class ClienteRepositoryJDBC implements IClienteRepositoryJDBC {
 
@@ -19,15 +23,22 @@ public class ClienteRepositoryJDBC implements IClienteRepositoryJDBC {
         this.jdbcTemplate = jdbcTemplate;
     }
 
+    /**
+     * Recupera todos os clientes do banco de dados usando uma consulta SQL simples.
+     *
+     * @return Uma lista de objetos Cliente contendo os dados dos clientes encontrados.
+     */
     public List<Cliente> findAllJDBC() {
+        // Define a consulta SQL para recuperar todos os clientes do banco de dados.
         String sql = "SELECT id, nome, rg FROM clientes";
+
+        // Executa a consulta SQL usando o jdbcTemplate e mapeia os resultados para objetos Cliente.
         return jdbcTemplate.query(sql, (rs, rowNum) -> {
-            Cliente pessoa = new Cliente();
-            pessoa.setId(rs.getLong("id"));
-            pessoa.setNome(rs.getString("nome"));
-            pessoa.setRg(rs.getString("rg"));
-            return pessoa;
+            Cliente cliente = new Cliente();
+            cliente.setId(rs.getLong("id"));
+            cliente.setNome(rs.getString("nome"));
+            cliente.setRg(rs.getString("rg"));
+            return cliente;
         });
     }
-	
 }
